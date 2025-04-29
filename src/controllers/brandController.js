784e37883cf134ce015b74dc3387f57fa6ddffe2
Beatriz L <158210831+limabea23@users.noreply.github.com>
2025-04-29 09:21:26 -0300

    feat: create controllers

---
+++ src/controllers/brandController.js
@@ -0,0 +1,60 @@
+const brandModel = require("../models/brandModel");
+
+const getAllBrands = async (req, res) => {
+    try {
+        const brands = await brandModel.getAllBrands();
+        res.json(brands);
+    } catch (error) {
+        res.status(500).json({ message: "erro ao buscar marca." });
+    }
+};
+
+const getBrand = async (req, res) => {
+    try {
+        const brand = await brandModel.getBrandById(req.params.id);
+        if (!brand) {
+            return res.status(404).json({ message: "marca não encontrada." });
+        }
+        res.json(brand);
+    } catch (error) {
+        res.status(500).json({ message: "erro ao buscar marca." });
+    }
+};
+
+const createBrand = async (req, res) => {
+    try {
+        const { name, founder } = req.body;
+        const newBrand = await brandModel.createBrand(name, founder);
+        res.status(201).json(newBrand);
+    } catch (error) {
+	console.log(error);
+        if (error.code === "23505") { // Código de erro do PostgreSQL para chave única violada
+            return res.status(400).json({ message: "marca já cadastrada." });
+        }
+        res.status(500).json({ message: "erro ao criar marca." });
+    }
+};
+
+const updateBrand = async (req, res) => {
+    try {
+        const { name, founder } = req.body;
+        const updatedBrand = await brandModel.updateBrand(req.params.id, name, founder);
+        if (!updatedBrand) {
+            return res.status(404).json({ message: "marca não encontrada." });
+        }
+        res.json(updatedBrand);
+    } catch (error) {
+        res.status(500).json({ message: "erro ao atualizar marca." });
+    }
+};
+
+const deleteBrand = async (req, res) => {
+    try {
+        const message = await brandModel.deleteBrand(req.params.id);
+        res.json(message);
+    } catch (error) {
+        res.status(500).json({ message: "erro ao deletar marca." });
+    }
+};
+
+module.exports = { getAllBrands, getBrand, createBrand, updateBrand, deleteBrand }
---
+++ src/controllers/cosmeticController.js
@@ -0,0 +1,56 @@
+const cosmeticModel = require("../models/cosmeticModel");
+
+const getAllCosmetics = async (req, res) => {
+    try {
+        const cosmetics = await wizardModel.getAllCosmetics();
+        res.json(cosmetics);
+    } catch (error) {
+        res.status(500).json({ message: "erro ao buscar cosméticos." });
+    }
+};
+
+const getCosmetic = async (req, res) => {
+    try {
+        const cosmetics = await cosmeticsModel.getCosmeticsById(req.params.id);
+        if (!cosmetics) {
+            return res.status(404).json({ message: "cosmético não encontrado." });
+        }
+        res.json(cosmetics);
+    } catch (error) {
+        res.status(500).json({ message: "erro ao buscar cosmético." });
+    }
+};
+
+const createCosmetic = async (req, res) => {
+    try {
+        const { name, category, price, description, brand_id } = req.body;
+        const newcosmetic = await cosmeticModel.createCosmetic(name, category, price, description, brand_id);
+        res.status(201).json(newcosmetic);
+    } catch (error) {
+        res.status(500).json({ message: "erro ao criar cosmético." });
+    }
+};
+
+const updateCosmetic = async (req, res) => {
+    try {
+        const { name, category, price, description, brand_id } = req.body;
+        const updatedCosmetic = await cosmeticModel.updateCosmetic(req.params.id, name, category, price, description, brand_id);
+        if (!updatedCosmetic) {
+            return res.status(404).json({ message: "cosmético não encontrado." });
+        }
+        res.json(updatedCosmetic);
+    } catch (error) {
+        res.status(500).json({ message: "erro ao atualizar cosmético." });
+    }
+};
+
+const deleteCosmetic = async (req, res) => {
+    try {
+        const message = await cosmeticModel.deleteCosmetic(req.params.id);
+        res.json(message);
+    } catch (error) {
+        res.status(500).json({ message: "erro ao deletar cosmético." });
+    }
+};
+
+module.exports = { getAllCosmetics, getCosmetic, createCosmetic, updateCosmetic, deleteCosmetic };
