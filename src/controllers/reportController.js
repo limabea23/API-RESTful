d186a7e0d7529dca2fca457e6b8d9e2af6028d69
Beatriz L <158210831+limabea23@users.noreply.github.com>
2025-04-29 11:49:13 -0300

    last commits of the day

--- src/controllers/reportController.js
+++ src/controllers/reportController.js
@@ -1,31 +1,7 @@
 const { format } = require("@fast-csv/format");
 const PDFDocument = require("pdfkit");
 
-const brandModel = require("../models/brandModel");
 const cosmeticModel = require("../models/cosmeticModel");
-
-const exportBrandCSV = async (req, res) => {
-    try {
-        const brand =  await brandModel.getBrand();
-
-        res.setHeader("Content-Disposition", "attachment; filename=brand.csv");
-        res.setHeader("Content-Type", "text-csv");
-
-        const csvStream = format({ headers: true});
-        csvStream.pipe(res);
-
-        brands.forEach((brand) => {
-            csvStream.write({
-                Nome: brand.name,
-                Fundador: brand.founder
-            });
-        });
-        
-        csvStream.end();
-    } catch (error) {
-        res.status(500).json({ message: "erro ao gerar o CSV"});
-    }
-};
 
 const exportCosmeticCSV = async (req, res) => {
     try {
@@ -50,37 +26,6 @@ const exportCosmeticCSV = async (req, res) => {
         csvStream.end();
     } catch (error) {
         res.status(500).json({ message: "erro ao gerar o CSV"});
-    }
-};
-
-const exportBrandPDF = async (req, res) => {
-    try {
-        const brands =  await brandModel.getBrand();
-
-        res.setHeader("Content-Type", "application/pdf");
-        res.setHeader("Content-Disposition", "inline; filename=brand.pdf")
-
-        const doc = new PDFDocument();
-        doc.pipe(res);
-
-        //Titulo
-        doc.fontSize(20).text("Relatório de Marcas", {align: "center"});
-        doc.moveDown();
-
-        //Cabeçalho
-        doc.fontSize(12).text("Nome | Fundador", {underline: true});
-        doc.moveDown(0.5);
-
-        //Add dados das marcas
-        brands.forEach((brand) => {
-            doc.text(
-                `${brand.name} | ${brand.founder} `
-            );
-        });
-
-        doc.end();
-    }  catch (error) {
-    res.status(500).json({ message: "erro ao gerar o PDF"}); 
     }
 };
 
@@ -115,4 +60,4 @@ const exportCosmeticPDF = async (req, res) => {
     }
 };
 
-module.exports = { exportBrandCSV, exportBrandPDF, exportCosmeticCSV, exportCosmeticPDF};
+module.exports = {  exportCosmeticCSV, exportCosmeticPDF};
--- src/routes/reportRoutes.js
+++ src/routes/reportRoutes.js
@@ -4,11 +4,9 @@ const router = express.Router();
 const reportController = require("./../controllers/reportController");
 
 //Rota para gerar CSV
-router.get("/report/csv", reportController.exportBrandCSV);
 router.get("/report/csv", reportController.exportCosmeticCSV);
 
 //Rota para o PDF
-router.get("/report/pdf/brand", reportController.exportBrandPDF);
 router.get("/report/pdf/cosmetic", reportController.exportCosmeticPDF);
 
 module.exports = router;
