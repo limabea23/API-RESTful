66d0c31ec7f51da5196118353c00bc3d252ba646
Beatriz L <158210831+limabea23@users.noreply.github.com>
2025-04-29 09:46:31 -0300

    feat: add brand and cosmetic routes

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const brandRoutes = require("./src/routes/brandRoutes.js");
+const cosmeticRoutes = require("./src/routes/cosmetic.js");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use("/api", brandRoutes); 
+app.use("/api", cosmeticRoutes); 
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
