958202629448c6c4235b0e177c69a7a18fe2a2f4
Beatriz L <158210831+limabea23@users.noreply.github.com>
2025-04-29 11:46:22 -0300

    feat: update photo

--- server.js
+++ server.js
@@ -2,9 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const brandRoutes = require("./src/routes/brandRoutes.js");
-const cosmeticRoutes = require("./src/routes/cosmeticRoutes.js");
+const cosmeticRoutes = require("./src/routes/cosmetic.js");
 const reportRoutes = require("./src/routes/reportRoutes.js");
-const path = require("path");
 
 const app = express();
 app.use(cors());
@@ -13,7 +12,6 @@ app.use(express.json());
 app.use("/api", brandRoutes); 
 app.use("/api", cosmeticRoutes); 
 app.use("/api", reportRoutes);
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
--- src/routes/brandRoutes.js
+++ src/routes/brandRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const brandController = require("../controllers/brandController");
+const upload = require("../config/upload.js");
 
 router.get("/brands", brandController.getAllBrands);
 router.get("/brands/:id", brandController.getBrand);
-router.post("/brands", brandController.createBrand);
+router.post("/brands", upload.single("photo"), brandController.createBrand);
 router.put("/brands/:id", brandController.updateBrand);
 router.delete("/brands/:id", brandController.deleteBrand);
 
