feb0f17b79efc1526ac2e66cc574eeb756abfa7d
Beatriz L <beatrizlima.tds1.senai@gmail.com>
2025-05-05 19:25:56 -0300

    feat: API Key with middleware

--- src/routes/brandRoutes.js
+++ src/routes/brandRoutes.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 const brandController = require("../controllers/brandController");
 const upload = require("../config/upload.js");
+const apiKeyMiddleware = require("../config/apiKey");
 
+router.use(apiKeyMiddleware);
 router.get("/brands", brandController.getAllBrands);
 router.get("/brands/:id", brandController.getBrand);
 router.post("/brands", upload.single("photo"), brandController.createBrand);
--- src/routes/cosmeticRoutes.js
+++ src/routes/cosmeticRoutes.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const cosmeticController = require("../controllers/cosmeticController");
+const apiKeyMiddleware = require("../config/apiKey");
 
+router.use(apiKeyMiddleware);
 router.get("/cosmetics", cosmeticController.getAllCosmetics);
 router.get("/cosmetics/:id", cosmeticController.getCosmetic);
 router.post("/cosmetics", cosmeticController.createCosmetic);
